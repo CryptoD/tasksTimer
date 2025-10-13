533ef48daf088c8e10df645d72799d56109b4cfa
CryptoD <ilw2vp7ymxa9@blurmail.net>
2025-10-13 15:05:27 +0400

    license update

--- tasksTimer@CryptoD/utils.js
+++ tasksTimer@CryptoD/utils.js
@@ -5,6 +5,8 @@
  * Provides helpers for internationalization, GNOME version detection,
  * logging, timeouts, intervals, command execution, and more.
  *
+ * Copyright (C) 2023 CryptoD
+ *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
